36bef3197ea9473ee4236c4dfa4b476401508f1f
BogdanTrenchev <117304421+Bogdan097@users.noreply.github.com>
2022-12-11 00:00:24 +0100

    Add files via upload

--- JS Homework #2/script.js
+++ JS Homework #2/script.js
@@ -1,6 +1,6 @@
 // // Write javascript code that will get an input from the user and calculate which Chinese Zodiac a given year is in
 
-// Formula for Chinese Zodiac caluclation: (year - 4) % 12. Possible values:
+// Formula for Chinese Zodiac calculation: (year - 4) % 12. Possible values:
 
 // 0-Rat      3-Rabbit   6-Horse   9-Rooster
 // 1-Ox       4-Dragon   7-Goat    10-Dog
@@ -44,4 +44,51 @@ else if ((yearBirth >= 1924 && yearBirth <= 2031) && chineseZodiac == 10){
 }
 else if ((yearBirth >= 1924 && yearBirth <= 2031) && chineseZodiac == 11){
     console.log("Based on your entry,your Zodiac sign is : Pig");
+}
+
+//With two prompts i can type in two values and get result from both ways, i can comment one or not,it works both ways...
+
+//Izgubiv nekoe vreme na case-ovite bidejki mi bea podredeni od case 1 natamu(a treba od case 0 bidejki Rat mi e 0 ,mislam deka e toa gledajki po toa deka uspea), i celo vreme sporedbite mi bea za edno podole mislam ,a treba da dobijam isti rezultati od dvata prompta...:)
+
+
+let yearBirth1 = parseInt(prompt("Enter your year"));
+let chineseZodiac1 = (yearBirth1 - 4 ) % 12;
+switch  (chineseZodiac1){
+    case 0:
+         console.log("Based on your entry,your Zodiac sign is :Rat");
+         break;
+    case 1:
+        console.log("Based on your entry,your Zodiac sign is : Ox");
+        break;
+    case 2 :
+         console.log("Based on your entry,your Zodiac sign is : Tiger");
+         break;
+     case 3 : 
+        console.log("Based on your entry,your Zodiac sign is : Rabbit");
+        break;
+     case 4 : 
+        console.log("Based on your entry,your Zodiac sign is : Dragon ");
+        break;
+     case 5: 
+        console.log("Based on your entry,your Zodiac sign is : Snake");
+        break;
+     case 6 :
+        console.log("Based on your entry,your Zodiac sign is : Horse");
+        break;
+     case 7 : 
+         console.log("Based on your entry,your Zodiac sign is : Goat");
+         break;
+     case 8 : 
+         console.log("Based on your entry,your Zodiac sign is : Monkey");
+         break; 
+     case 9 : 
+         console.log("Based on your entry,your Zodiac sign is : Rooster");
+         break;  
+     case 10 : 
+         console.log("Based on your entry,your Zodiac sign is : Dog");
+         break;
+     case 11 : 
+         console.log("Based on your entry,your Zodiac sign is : Pig");
+         break;          
+                
 }
